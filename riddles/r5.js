3ec506b76839aaeaffd974b6f0c313dbd2e99489
israelr9897 <israelr9897@gmail.com>
2025-06-29 15:34:54 +0300

    add hints to all riddles

--- riddles/r5.js
+++ riddles/r5.js
@@ -3,6 +3,7 @@ export default {
     level: "hard",
     name: "hard Math",
     timeLimit: 7,
+    hint: "between 55 - 75",
     taskDescription: "What is 4 to the power of 3 (4 ** 3) ?",
     correctAnswer: "64"
   };
