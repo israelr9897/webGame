edf9adbab8243fd96a181e120da6f93467c87f9b
israelr9897 <israelr9897@gmail.com>
2025-07-07 16:23:04 +0300

    add get riddles and add riddel to server from db

---
+++ serviceToServer/serviceGame.js
@@ -0,0 +1,71 @@
+import rl from "readline-sync";
+import {
+  getRiddels,
+  ALLRIDDLES,
+  deleteRiddle,
+  PrintAllRiddles,
+  checkLevelSelction,
+  serviceCreateRiddle,
+  updateRiddle,
+  initObjRiddle
+} from "./serviceRiddle.js";
+import { getPlayerObj } from "./servicePlayer.js";
+
+async function flowGame() {
+  let player = await welcome()
+  let level = checkLevelSelction();
+  console.log("\nLet's get started.");
+  while (true) {
+    const listOfRiddles = initObjRiddle(level);
+    console.log( listOfRiddles);
+    listOfRiddles.forEach((riddle) => riddle.startAsk(player));
+    const choice = rl
+    .question("Would you like to continue to another level? (y/n)")
+    .toLowerCase();
+    if (choice === "n") {
+      break;
+    }
+    console.log("Choose difficulty: easy / medium / hard:\n");
+    level = checkLevelSelction();
+  }
+  console.log("We're done, well done Aniat for all the riddels!\n");
+  player.showStats();
+  player = isLowerTime(player);
+  const players = await updateDataToPlayer(player);
+  await createPlayer(players);
+}
+
+async function MannegerGame(choice) {
+  await getRiddels()
+  switch (choice) {
+    case "1":
+      flowGame();
+      break;
+
+    case "2":
+      serviceCreateRiddle();
+      break;
+
+    case "3":
+      PrintAllRiddles();
+      break;
+
+    case "4":
+      updateRiddle();
+      break;
+
+    case "5":
+      deleteRiddle();
+      break;
+  }
+}
+
+async function welcome() {
+  const name = "";
+  console.log("---------- Welcome to the game ----------\n");
+  const id = rl.question("Enter your ID, Or Enter 00 To register: ");
+  const player = await getPlayerObj(id);
+  return player;
+}
+
+export { initObjRiddle, MannegerGame, welcome };
---
+++ serviceToServer/serviceRiddle.js
@@ -0,0 +1,149 @@
+import rl from "readline-sync";
+import { writeRiddleInDB, readFileToRiddles } from "../db/DALriddles.js";
+
+let ALLRIDDLES = [];
+
+async function getRiddels() {
+  ALLRIDDLES = await readFileToRiddles();
+}
+
+async function serviceCreateRiddle(){
+  const ridlle = CreateRiddleObj();
+  ridlle.id = ALLRIDDLES.length + 1;
+  ALLRIDDLES.push(ridlle);
+  await writeRiddleInDB(ALLRIDDLES);
+}
+
+function checkLevelSelction() {
+  const listOfLevels = ["easy", "medium", "hard"];
+  console.log("Enter the riddle level (easy / medium / hard):  ");
+  let choice = rl.question().toLowerCase();
+  while (!listOfLevels.includes(choice)) {
+    console.log("Invalid selection");
+    choice = rl
+      .question("Please select again easy / medium / hard\n")
+      .toLowerCase();
+  }
+  return choice;
+}
+
+function checkUpdateSelction() {
+  const listOfFilds = ["level","name","timeLimit","hint","taskDescription","correctAnswer"];
+  console.log("Enter the riddle chenge (level / name / timeLimit / hint / taskDescription / correctAnswer) :");
+  let choice = rl.question().toLowerCase();
+  while (!listOfFilds.includes(choice)) {
+    console.log("Invalid selection");
+    choice = rl.question("Please select again level / name / timeLimit / hint / taskDescription / correctAnswer\n").toLowerCase();
+  }
+  return choice;
+}
+
+function CreateRiddleObj() {
+  const riddleObj = {
+    "id" : 0,
+    "level": checkLevelSelction(),
+    "name": rl.question("Enter name of the riddle: "),
+    "timeLimit": rl.question("Enter time limit of the riddle: "),
+    "taskDescription": rl.question("Enter the riddle: "),
+    "hint": rl.question("Enter hint of the riddle: "),
+    "correctAnswer": rl.question("Enter the answer of the riddle: "),
+  };
+  return riddleObj;
+}
+const URL = "http://localhost:3100";
+
+async function PrintAllRiddles(){
+  const riddles = ((await fetch(URL + "/riddle")).json());
+  console.log(await riddles);
+}
+
+async function addRiddle(obj) {
+  const da = ((await fetch(URL + "/riddles/addRiddle", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": "reqres-free-v1",
+      },
+      body: JSON.stringify(obj),
+    })).text());
+    console.log(await da);
+}
+addRiddle(CreateRiddleObj());
+// PrintAllRiddles()
+function changeFromUserToRiddle(riddle){
+  let isExit = false;
+  while(!isExit){
+    console.log("What do you want to change?");
+    const input = checkUpdateSelction();
+    const newContent = rl.question("Insert the new content: ");
+    riddle[input] = newContent;
+    const mroeChenge = rl.question("Do you want to make more changes? (y/n) ").toLowerCase();
+    if(mroeChenge === "n"){
+      isExit = true;
+    }
+  }
+  console.log("Changes saved successfully");
+}
+
+async function updateRiddle() {
+  console.log(ALLRIDDLES);
+  const id = Number(rl.question("Enter the riddle ID. "));
+  ALLRIDDLES.forEach(riddle => {
+    if(riddle.id === id){
+      console.log(riddle);
+      riddle = changeFromUserToRiddle(riddle);
+    }
+  });
+  writeRiddleInDB(ALLRIDDLES);
+}
+
+async function deleteRiddle() {
+  console.log(ALLRIDDLES);
+  const id = Number(rl.question("Enter the riddle ID. "));
+  ALLRIDDLES.forEach(riddle => {
+    if(riddle.id === id){
+      console.log(riddle);
+    }
+  });
+  const input = rl.question("Are you sure you want to delete? (y/n) : ").toLowerCase();
+  if(input === "n"){
+    console.log("Good by");
+    return;
+  }
+  else if(input === "y"){
+    ALLRIDDLES = ALLRIDDLES.filter(riddle => riddle.id !== id);
+    writeRiddleInDB(ALLRIDDLES);
+  }
+}
+
+function initObjRiddle(level) {
+  let listOfRiddles = [];
+  switch (level) {
+    case "easy":
+      listOfRiddles = ALLRIDDLES.map((riddle) => {
+        if (riddle.level === "easy") {
+          return new Riddle(riddle);
+        }
+      }).filter((ridlle) => ridlle);
+      return listOfRiddles;
+
+    case "medium":
+      listOfRiddles = ALLRIDDLES.map((riddle) => {
+        if (riddle.level === "medium") {
+          return new Riddle(riddle);
+        }
+      }).filter((ridlle) => ridlle);
+      return listOfRiddles;
+
+    case "hard":
+      listOfRiddles = ALLRIDDLES.map((riddle) => {
+        if (riddle.level === "hard") {
+          return new Riddle(riddle);
+        }
+      }).filter((ridlle) => ridlle);
+      return listOfRiddles;
+  }
+}
+
+
+export {getRiddels, ALLRIDDLES, checkLevelSelction, serviceCreateRiddle, PrintAllRiddles, updateRiddle, deleteRiddle, initObjRiddle};
