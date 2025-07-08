ba40c7d8a9aef6cdf27a42b1205e19a166d6f1a8
israelr9897 <israelr9897@gmail.com>
2025-07-08 17:56:15 +0300

    added functions to update and delete riddle to server

--- serviceToServer/serviceRiddle.js
+++ serviceToServer/serviceRiddle.js
@@ -1,13 +1,14 @@
 import rl from "readline-sync";
 import { writeRiddleInDB, readFileToRiddles } from "../db/DALriddles.js";
-import {Riddle} from "../classes/Riddle.js";
+import { Riddle } from "../classes/Riddle.js";
+import { json } from "express";
 
 const URL = "http://localhost:3100";
 let ALLRIDDLES = [];
 
 async function getRiddels() {
-  ALLRIDDLES = ((await fetch(URL + "/riddle")).json());
-  console.log(await ALLRIDDLES);
+  const response = await fetch(URL + "/riddle");
+  ALLRIDDLES = await response.json();
 }
 
 // async function serviceCreateRiddle(){
@@ -31,70 +32,98 @@ function checkLevelSelction() {
 }
 
 function checkUpdateSelction() {
-  const listOfFilds = ["level","name","timeLimit","hint","taskDescription","correctAnswer"];
-  console.log("Enter the riddle chenge (level / name / timeLimit / hint / taskDescription / correctAnswer) :");
+  const listOfFilds = [
+    "level",
+    "name",
+    "timeLimit",
+    "hint",
+    "taskDescription",
+    "correctAnswer",
+  ];
+  console.log(
+    "Enter the riddle chenge (level / name / timeLimit / hint / taskDescription / correctAnswer) :"
+  );
   let choice = rl.question().toLowerCase();
   while (!listOfFilds.includes(choice)) {
     console.log("Invalid selection");
-    choice = rl.question("Please select again level / name / timeLimit / hint / taskDescription / correctAnswer\n").toLowerCase();
+    choice = rl
+      .question(
+        "Please select again level / name / timeLimit / hint / taskDescription / correctAnswer\n"
+      )
+      .toLowerCase();
   }
   return choice;
 }
 
 function CreateRiddleObj() {
   const riddleObj = {
-    "id" : 0,
-    "level": checkLevelSelction(),
-    "name": rl.question("Enter name of the riddle: "),
-    "timeLimit": rl.question("Enter time limit of the riddle: "),
-    "taskDescription": rl.question("Enter the riddle: "),
-    "hint": rl.question("Enter hint of the riddle: "),
-    "correctAnswer": rl.question("Enter the answer of the riddle: "),
+    id: 0,
+    level: checkLevelSelction(),
+    name: rl.question("Enter name of the riddle: "),
+    timeLimit: rl.question("Enter time limit of the riddle: "),
+    taskDescription: rl.question("Enter the riddle: "),
+    hint: rl.question("Enter hint of the riddle: "),
+    correctAnswer: rl.question("Enter the answer of the riddle: "),
   };
   return riddleObj;
 }
 
-async function PrintAllRiddles(){
-  const riddles = ((await fetch(URL + "/riddle")).json());
-  console.log(await riddles);
+async function PrintAllRiddles() {
+  const response = await fetch(URL + "/riddle");
+  const riddles = await response.json();
+  console.log(riddles);
 }
 
 async function addRiddle(newRiddle) {
-  const da = ((await fetch(URL + "/riddles/addRiddle", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "x-api-key": "reqres-free-v1",
-      },
-      body: JSON.stringify(newRiddle),
-    })).text());
-    console.log(await da);
+  const response = await fetch(URL + "/riddles/addRiddle", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newRiddle),
+  });
+  const data = await response.json();
+  console.log(data);
 }
 
 async function updateRiddleToServer(riddle) {
-  fetch(URL + "/riddles/updateRiddle")
-  const da = ((await fetch(URL + "/riddles/updateRiddle", {
+  const response = await fetch(URL + "/riddles/updateRiddle", {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
-      "x-api-key": "reqres-free-v1",
     },
     body: JSON.stringify(riddle),
-  })).text());
-  console.log(await da);
+  });
+  const data = await response.json();
+  console.log(data);
+}
+async function deleteToServer(id){
+  const response = await fetch(URL + "/riddles/deleteRiddle", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({idRiddle: id}),
+  });
+  const data = await response.json();
+  console.log(data);
+
 }
 // addRiddle(CreateRiddleObj());
-// PrintAllRiddles()
-updateRiddle();
-function changeFromUserToRiddle(riddle){
+// PrintAllRiddles();
+// updateRiddle();
+deleteRiddle()
+function changeFromUserToRiddle(riddle) {
   let isExit = false;
-  while(!isExit){
+  while (!isExit) {
     console.log("What do you want to change?");
     const input = checkUpdateSelction();
     const newContent = rl.question("Insert the new content: ");
     riddle[input] = newContent;
-    const mroeChenge = rl.question("Do you want to make more changes? (y/n) ").toLowerCase();
-    if(mroeChenge === "n"){
+    const mroeChenge = rl
+      .question("Do you want to make more changes? (y/n) ")
+      .toLowerCase();
+    if (mroeChenge === "n") {
       isExit = true;
     }
   }
@@ -103,33 +132,38 @@ function changeFromUserToRiddle(riddle){
 }
 
 async function updateRiddle() {
-  await getRiddels()
+  await getRiddels();
+  console.log(ALLRIDDLES);
   const id = Number(rl.question("Enter the riddle ID. "));
-  ALLRIDDLES.forEach(riddle => {
-    if(riddle.id === id){
-      console.log(riddle);
-      riddle = changeFromUserToRiddle(riddle);
-      updateRiddleToServer(riddle);
-    }
-  });
+  let riddle = ALLRIDDLES.find((r) => r.id === id);
+  if (riddle) {
+    const newRiddle = changeFromUserToRiddle(riddle);
+    Object.assign(riddle, newRiddle);
+    updateRiddleToServer(newRiddle);
+    return;
+  }
+  console.log("The ID you selected does not exist.");
 }
 
 async function deleteRiddle() {
+  await getRiddels()
   console.log(ALLRIDDLES);
   const id = Number(rl.question("Enter the riddle ID. "));
-  ALLRIDDLES.forEach(riddle => {
-    if(riddle.id === id){
+  ALLRIDDLES.forEach((riddle) => {
+    if (riddle.id === id) {
       console.log(riddle);
     }
   });
-  const input = rl.question("Are you sure you want to delete? (y/n) : ").toLowerCase();
-  if(input === "n"){
+  const input = rl
+    .question("Are you sure you want to delete? (y/n) : ")
+    .toLowerCase();
+  if (input === "n") {
     console.log("Good by");
     return;
-  }
-  else if(input === "y"){
-    ALLRIDDLES = ALLRIDDLES.filter(riddle => riddle.id !== id);
-    writeRiddleInDB(ALLRIDDLES);
+  } else if (input === "y") {
+    ALLRIDDLES = ALLRIDDLES.filter((riddle) => riddle.id !== id);
+    // writeRiddleInDB(ALLRIDDLES);
+    deleteToServer(id);
   }
 }
 
@@ -162,5 +196,12 @@ function initObjRiddle(level) {
   }
 }
 
-
-export {getRiddels, ALLRIDDLES, checkLevelSelction, PrintAllRiddles, updateRiddle, deleteRiddle, initObjRiddle};
+export {
+  getRiddels,
+  ALLRIDDLES,
+  checkLevelSelction,
+  PrintAllRiddles,
+  updateRiddle,
+  deleteRiddle,
+  initObjRiddle,
+};
