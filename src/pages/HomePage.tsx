076640d84e3009be54f9847d2120b88e615c4830
israelr9897 <israelr9897@gmail.com>
2025-09-02 17:52:09 +0300

    fixed errors

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,4 @@
 import ContentHome from "../compes/ContentHome";
-import Footer from "../compes/Footer";
-import Header from "../compes/header";
 import "../styles/home.css";
 
 export default function HomePage() {
